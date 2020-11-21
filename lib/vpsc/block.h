cfd98f77184431d7d08ca6b27040829f7cc210a0
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-11-20 17:12:50 -0800

    remove unnecessary explicit void in VPSC
    Unlike in C, prototypes with no parameter list are not ambiguous in C++.

--- lib/vpsc/block.h
+++ lib/vpsc/block.h
@@ -35,7 +35,7 @@ public:
 	double weight;
 	double wposn;
 	Block(Variable *v=NULL);
-	~Block(void);
+	~Block();
 	Constraint* findMinLM();
 	Constraint* findMinLMBetween(Variable* lv, Variable* rv);
 	Constraint* findMinInConstraint();
