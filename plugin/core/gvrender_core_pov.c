449fab2281928c5823b683694f20f5758e1f88a9
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-03-30 12:13:10 -0700

    core plugin pov_polygon: remove intermediate 'x' buffer
    As `agxbuf`s support appending to the existing content, we can avoid this double
    buffering.

--- plugin/core/gvrender_core_pov.c
+++ plugin/core/gvrender_core_pov.c
@@ -697,20 +697,18 @@ static void pov_polygon(GVJ_t * job, pointf * A, int n, int filled)
 	agxbuf pov = {0};
 	agxbprint(&pov, POV_SPHERE_SWEEP, "linear_spline", n + 1);
 
-	agxbuf v = {0}, x = {0};
+	agxbuf v = {0};
 	for (i = 0; i < n; i++) {
 		agxbprint(&v, POV_VECTOR3 ", %.3f\n", A[i].x + job->translation.x,
 		          A[i].y + job->translation.y, 0.0, job->obj->penwidth); // z coordinate, thickness
-		agxbprint(&x, "%s    %s", agxbuse(&pov), agxbuse(&v)); // catenate pov & vector v
-		agxbput(&pov, agxbuse(&x));
+		agxbprint(&pov, "    %s", agxbuse(&v)); // catenate pov & vector v
 	}
 
 	//close polygon, add starting point as final point^
 	agxbprint(&v, POV_VECTOR3 ", %.3f\n", A[0].x + job->translation.x,
 	          A[0].y + job->translation.y, 0.0, job->obj->penwidth); // z coordinate, thickness
 
-	agxbprint(&x, "%s    %s", agxbuse(&pov), agxbuse(&v)); // catenate pov & vector v
-	agxbput(&pov, agxbuse(&x));
+	agxbprint(&pov, "    %s", agxbuse(&v)); // catenate pov & vector v
 
 	gvprintf(job, "%s    tolerance 0.1\n    %s    %s    %s    %s" END,
 	         agxbuse(&pov), agxbuse(&s), agxbuse(&r), agxbuse(&t), p);
@@ -731,8 +729,7 @@ static void pov_polygon(GVJ_t * job, pointf * A, int n, int filled)
 			agxbprint(&v, POV_VECTOR3,
 			       A[i].x + job->translation.x,
 			       A[i].y + job->translation.y, 0.0);
-			agxbprint(&x, "%s\n    %s", agxbuse(&pov), agxbuse(&v)); // catenate pov & vector v
-			agxbput(&pov, agxbuse(&x));
+			agxbprint(&pov, "\n    %s", agxbuse(&v)); // catenate pov & vector v
 		}
 		gvprintf(job, "%s\n    %s    %s    %s    %s" END, agxbuse(&pov),
 		         agxbuse(&s), agxbuse(&r), agxbuse(&t), p);
@@ -740,7 +737,6 @@ static void pov_polygon(GVJ_t * job, pointf * A, int n, int filled)
 		free(p);
 	}
 	agxbfree(&v);
-	agxbfree(&x);
 	agxbfree(&s);
 	agxbfree(&r);
 	agxbfree(&t);
