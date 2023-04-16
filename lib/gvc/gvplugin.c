stinb/graphviz

commit d4bf84bc45a9b5b35ca435e58fa0e3b0bf32f326

gvplugin_list: remove unnecessary duplication of 'str'
By managing an extent into the original string, we can avoid a dynamic
allocation in this code. This is similar in spirit to
5a13ab3f08b2066a795ea152f9594f6719d3aaa6.

lib/gvc/gvplugin.c
@@ -364,7 +364,7 @@ char *gvplugin_list(GVC_t * gvc, api_t api, const char *str)
     static int first = 1;
     const gvplugin_available_t *pnext, *plugin;
     char *bp;
-    char *s, *p, *q, *typestr_last;
+    char *p, *q, *typestr_last;
     bool new = true;
     static agxbuf xb;
 
@@ -378,22 +378,26 @@ char *gvplugin_list(GVC_t * gvc, api_t api, const char *str)
     }
 
     /* does str have a :path modifier? */
-    s = strdup(str);
-    p = strchr(s, ':');
-    if (p)
-        *p++ = '\0';
+    size_t str_size = strlen(str);
+    {
+        const char *end = strchr(str, ':');
+        if (end != NULL) {
+            str_size = (size_t)(end - str);
+        }
+    }
 
     /* point to the beginning of the linked list of plugins for this api */
     plugin = gvc->apis[api];
 
-    if (p) {                    /* if str contains a ':', and if we find a match for the type,
+    if (str[str_size] == ':') { /* if str contains a ':', and if we find a match for the type,
                                    then just list the alternative paths for the plugin */
         for (pnext = plugin; pnext; pnext = pnext->next) {
             q = strdup(pnext->typestr);
             if ((p = strchr(q, ':')))
                 *p++ = '\0';
             /* list only the matching type, or all types if s is an empty string */
-            if (!s[0] || strcasecmp(s, q) == 0) {
+            if (!str[0] ||
+                (strlen(q) == str_size && strncasecmp(str, q, str_size) == 0)) {
                 /* list each member of the matching type as "type:path" */
                 agxbprint(&xb, " %s:%s", pnext->typestr, pnext->package->name);
                 new = false;
@@ -401,7 +405,6 @@ char *gvplugin_list(GVC_t * gvc, api_t api, const char *str)
             free(q);
         }
     }
-    free(s);
     if (new) {                  /* if the type was not found, or if str without ':',
                                    then just list available types */
         typestr_last = NULL;

commit 75f4c70685f18502d29666c828319e555138016c

gvc gvplugin_list: fix incorrect plugin list for call with ":"
When calling `gvplugin_list` with the string `":"`, plugins would be returned
with their truncated names. E.g. “bmp” instead of “bmp:quartz:quartz”. This bug
was inadvertently introduced in d4bf84bc45a9b5b35ca435e58fa0e3b0bf32f326.

Thanks to Ryan Schmidt and Brendan Shanks for guiding me to a solution here.

Gitlab: fixes #2375
MacPorts: https://trac.macports.org/ticket/67226
Reported-by: Ryan Schmidt
Tested-by: Brendan Shanks

lib/gvc/gvplugin.c
@@ -353,8 +353,9 @@ char *gvplugin_list(GVC_t * gvc, api_t api, const char *str)
                                           then just list the alternative paths for the plugin */
         for (pnext = plugin; pnext; pnext = pnext->next) {
             const strview_t type = strview(pnext->typestr, ':');
-            /* list only the matching type, or all types if s is an empty string */
-            if (!str[0] || strview_case_eq(strv, type)) {
+            // list only the matching type, or all types if str is an empty
+            // string or starts with ":"
+            if (strv.size == 0 || strview_case_eq(strv, type)) {
                 /* list each member of the matching type as "type:path" */
                 agxbprint(&xb, " %s:%s", pnext->typestr, pnext->package->name);
                 new = false;

tests/test_regression.py
@@ -2678,7 +2678,6 @@ def test_2371():
     platform.system() == "Windows",
     reason="gvplugin_list symbol is not exposed on Windows",
 )
-@pytest.mark.xfail(strict=True)
 def test_2375():
     """
     `gvplugin_list` should return full plugin names