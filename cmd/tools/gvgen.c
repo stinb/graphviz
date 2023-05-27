fa94aaf0992cdf440fd51755e6761fe69971b2e2
Matthew Fernandez <matthew.fernandez@gmail.com>
2023-05-26 18:24:55 -0700

    gvgen: abbrevate 'setTwoTwoOpt'
    This looks like it changes the result when `readPos` returns 0. But `readPos`
    only ever returns -1 or ≥1.

--- cmd/tools/gvgen.c
+++ cmd/tools/gvgen.c
@@ -207,12 +207,7 @@ static int setTwoTwoOpt(char *s, opts_t* opts, int dflt)
     }
 
     s = next + 1;
-    d = readPos(s, &next);
-    if (d < 0) {
-	return d;
-    }
-    opts->parm2 = d;
-    return 0;
+    return readOne(s, &opts->parm2);
 }
 
 /* setTwoOpt:
